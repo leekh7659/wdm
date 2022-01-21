45f84c105c9f1b6fceab6609f5401942c0ad7593
root <root@ip-10-0-0-41.ap-northeast-2.compute.internal>
2022-01-21 05:50:21 +0000

    fist commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php echo"eeee";?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>wdm</title>
+</head>
+<body>
+<h1>Welcome my baby</h1>
+</body>
+</html>
---
+++ phpinfo.php
@@ -0,0 +1,3 @@
+<?php
+var_dump('wdm will be best platform');
+?>
